9db2f74a96d5ca1033187725c6c4afc649e3529a
Tomer Fishaimer <ftom684@gmail.com>
2023-02-19 00:42:22 +0200

    SMS Authentication - Sent - Front
    Resolves: MIT-6

---
+++ app/auth/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import VerificationInput from "react-verification-input";
+import Button from "../components/shared/Button";
+import PageTitle from "../components/shared/PageTitle";
+
+export default function Approve() {
+  const [value, setValue] = useState("");
+  return (
+    <div className="h-full flex flex-col justify-center items-center px-5 w-full">
+      <div className="text-center">
+        <PageTitle className="text-2xl">התחברות</PageTitle>
+        <p className="mt-8">שלחנו קוד אימות ב-SMS למספר</p>
+        <p className="mt-2 font-bold">052-3554728</p>
+        <p className="mt-5">יש להזין אותו כעת:</p>
+
+        <div dir="ltr" className="mt-2 h-20">
+          <VerificationInput
+            value={value}
+            autoFocus
+            validChars="0-9"
+            inputProps={{ type: "tel", autoComplete: "one-time-code" }}
+            length={5}
+            placeholder=""
+            classNames={{
+              container: "container",
+              character:
+                "rounded-full w-12 h-20 border border-green-light flex items-center justify-center",
+              characterInactive: "bg-white",
+              characterSelected: "bg-green-light"
+            }}
+            onChange={(newVal) => setValue(newVal)}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-full text-center mt-20">
+        <Button>אישור</Button>
+        <a href="#" className="mt-2 underline">
+          שלחו לי שוב את הקוד
+        </a>
+      </div>
+    </div>
+  );
+}
