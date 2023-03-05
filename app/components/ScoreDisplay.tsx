b0a3de44a61be035857f09fe3347979506884780
Noam <noambar2845@gmail.com>
2023-03-05 18:48:10 +0200

    Changing to UI

--- app/components/ScoreDisplay.tsx
+++ app/components/ScoreDisplay.tsx
@@ -1,39 +1,30 @@
-import Image from "next/image";
+import LeafIcon from "public/images/leaf-icon.svg";
+import GreenLeafIcon from "public/images/leaf-icon-green.svg";
+import CoinIcon from "public/images/coin-icon.svg";
 
 export interface IScoreDisplayProps {
-  bgColor: string;
-  borderColor: string;
-  textColor: string;
   score: number;
-  src: string;
+  isBold: boolean;
+  isLeaf: boolean;
 }
 
 export default function ScoreDisplay({
-  bgColor,
-  borderColor,
-  textColor,
   score,
-  src,
+  isBold,
+  isLeaf,
 }: IScoreDisplayProps) {
   return (
-    <div className="flex relative">
-      <div
-        className="w-[75px] flex items-center relative h-6 rounded-xl"
-        style={{
-          backgroundColor: bgColor,
-          borderColor: borderColor,
-          color: textColor,
-        }}
-      >
-        <p className="pr-4">{score}</p>
-      </div>
-      <Image
-        className="absolute left-0 -translate-x-[20%] "
-        src={`/images/${src}`}
-        alt=""
-        height={26}
-        width={33}
-      />
+    <div className="flex items-center gap-x-1">
+      {isBold ? <p className="font-bold">{score}</p> : <p>{score}</p>}
+      {isLeaf ? (
+        isBold ? (
+          <GreenLeafIcon className="h-4 w-4" />
+        ) : (
+          <LeafIcon className="h-6 w-6" />
+        )
+      ) : (
+        <CoinIcon className="h-4 w-4" />
+      )}
     </div>
   );
 }
