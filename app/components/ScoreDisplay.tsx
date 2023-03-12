d8a92e9ef05c2b4f59262cda2eae2269739aa697
Noam <noambar2845@gmail.com>
2023-03-12 16:28:31 +0200

    MIT-21 colors added, header, hero svgs - cloud, logo+text, mityaalim logo. also contains ScoreDisplay

---
+++ app/components/ScoreDisplay.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Image from "next/image";
+import LeafIcon from "public/images/leaf-icon.svg";
+import { ReactElement } from "react";
+
+export interface IScoreDisplayProps {
+  score: number;
+  style: string | null;
+  icon?: ReactElement | null;
+}
+
+export default function ScoreDisplay({
+  score,
+  style = null,
+  icon = null,
+}: IScoreDisplayProps) {
+  const IconComponent = () => icon;
+  return (
+    <div className={`flex items-center gap-x-1 text-base ${style}`}>
+      <span>{score}</span>
+      <span className="w-6 h-6 ml-1">
+        {icon === null ? <LeafIcon /> : <IconComponent />}
+      </span>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,18 +14,22 @@ module.exports = {
           250: "#C7D2D0",
           550: "#9F9F9F",
           650: "#79747E",
-          dark: "#3C3C3C"
+          dark: "#3C3C3C",
         },
         green: {
           light: "#D3EAD3",
           primary: "#22AA22",
-          dark: "#134239"
-        }
+          progressBarIncome: "#93D193",
+          dark: "#134239",
+        },
+      },
+      boxShadow: {
+        main: " 0px 4px 24px rgba(0, 0, 0, 0.08)",
       },
       fontFamily: {
-        primary: ["var(--font-open-sans)"]
-      }
-    }
+        primary: ["var(--font-open-sans)"],
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
