09630278dc1e3fd1c28e1944a233ba5b6a9eb53a
Tomer Fishaimer <ftom684@gmail.com>
2023-03-16 18:08:32 +0200

    UI | Create Savings Goal page
    Resolves: MIT-101

--- app/personal-info/ProgressBar.tsx
+++ app/personal-info/ProgressBar.tsx
@@ -3,20 +3,25 @@ interface IProgressBarProps {
 }
 function ProgressBar({ step }: IProgressBarProps) {
   return (
-    <div className="progress bg-green-100 flex h-1 rounded-full overflow-hidden mb-5 w-full">
-      <span
-        className={`flex-1 ${
-          step === 1 ? "rounded-l-full bg-green-300" : "bg-green-300"
-        }`}
-      />
-      <span
-        className={`flex-1 ${step === 2 ? "rounded-l-full bg-green-300" : ""}
+    <>
+      <div className="flex justify-center text-gray-400 text-sm">{step}/3</div>
+      <div className="progress bg-green-100 flex h-1 rounded-full overflow-hidden mb-5 w-full">
+        <span
+          className={`flex-1 ${
+            step === 1 ? "rounded-l-full bg-green-300" : "bg-green-300"
+          }`}
+        />
+        <span
+          className={`flex-1 ${step === 2 ? "rounded-l-full bg-green-300" : ""}
         ${step > 2 ? "bg-green-300" : ""}`}
-      />
-      <span
-        className={`flex-1 ${step === 3 ? "rounded-l-full bg-green-300" : ""}`}
-      />
-    </div>
+        />
+        <span
+          className={`flex-1 ${
+            step === 3 ? "rounded-l-full bg-green-300" : ""
+          }`}
+        />
+      </div>
+    </>
   );
 }
 export default ProgressBar;
